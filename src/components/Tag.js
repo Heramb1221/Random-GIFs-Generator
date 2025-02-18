e4441722dee031bcac2d0a52461ed941994bc3e7
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-02-18 08:53:24 +0530

    custom hook

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,44 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import React, { useState } from 'react'
 import Spinner from './Spinner';
+import useGif from '../hooks/useGif';
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;
 
 const Tag = () => {
 
-    const [gif,setGif] = useState('');
-    const [loading, setLoading] = useState(false);
     const[tag, setTag] = useState('');
-
-    async function fetchData() {
-        setLoading(true);
-        const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
-        const { data } = await axios.get(url);
-        const imageSource = data.data.images.downsized_large.url;
-        setGif(imageSource);
-        setLoading(false);
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, [])
-
-    function clickHandler() {
-        fetchData();
-    }
-
-    function changeHandler(event) {
-        setTag(event.target.vaule);
-    }
+    const {gif, loading, fetchData} = useGif(tag);
 
   return (
     <div className='w-1/2 bg-blue-500 rounded-lg border border-black flex flex-col items-center gap-y-5 mt-[15px]'>
-        <h1 className='mt-[15px] text-2xl underline uppercase font-bold'>Random GIF</h1>
+        <h1 className='mt-[15px] text-2xl underline uppercase font-bold'>Random {tag} GIF</h1>
         {
             loading ? (<Spinner/>) : (<img src={gif} width="450"/>)
         }
-        <input className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center' onChange={changeHandler} value={tag}></input>
-        <button onClick={clickHandler} className='mb-[20px] w-10/12 bg-yellow-500 text-lg py-2 rounded-lg'>Generate</button>
+        <input className='w-10/12 text-lg py-2 rounded-lg mb-[3px] text-center' onChange={(event) => setTag(event.target.value)} value={tag}></input>
+        <button onClick={() => fetchData(tag)} className='mb-[20px] w-10/12 bg-yellow-500 text-lg py-2 rounded-lg'>Generate</button>
     </div>
   )
 }
